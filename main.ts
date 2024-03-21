b9df9b0187854db909583aa540633a810e6c7b1a
Eduardo Rangell <rangellferreira@gmail.com>
2024-03-20 21:45:41 -0300

    created routes

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from './generated/client/deno/edge.ts'
+import { PrismaClient } from "./generated/client/deno/edge.ts";
 import { Application, Router } from "jsr:@oak/oak@14";
 
 import { load } from "https://deno.land/std@0.220.1/dotenv/mod.ts";
@@ -17,7 +17,45 @@ const app = new Application();
 const router = new Router();
 
 router
-  .get('/', (context) => {
-    context.response.body = "Bem vindo a API do Crachá"
+  .get("/", (context) => {
+    context.response.body = "Bem vindo a API do Crachá";
   })
-  .get('/visitante', (context) => {})
+  .get("/visitantes", async (context) => {
+    const visitantes = await prisma.visitante.findMany();
+    context.response.body = visitantes;
+  })
+  .get("/visitate/:id", async (context) => {
+    const { id } = context.params;
+    const visitante = await prisma.visitante.findUnique({
+      where: {
+        id: Number(id),
+      },
+    });
+    context.response.body = visitante;
+  })
+  .post("/visitante", async (context) => {
+    const { nome, empresa, email } = await context.request.body.json();
+    const result = await prisma.visitante.create({
+      data: {
+        nome: nome,
+        empresa: empresa,
+        email: email,
+        printed: true,
+      },
+    });
+    context.response.body = result;
+  })
+  .delete("/visitante/:id", async (context) => {
+    const { id } = context.params;
+    const visitante = await prisma.visitante.delete({
+      where: {
+        id: Number(id),
+      },
+    });
+    context.response.body = visitante;
+  });
+
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+await app.listen({ port: 8000 });
